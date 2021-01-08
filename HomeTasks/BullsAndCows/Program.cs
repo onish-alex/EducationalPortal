5d0cfc4bdddff09f58b4b43e9432474c78cef16a
onish-alex <onish.alex2000@gmail.com>
2021-01-08 02:25:22 +0200

    Added "Bulls and Cows" home task

---
+++ HomeTasks/BullsAndCows/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BullsAndCows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var view = new ConsoleView(new GameLogic());
+        }
+    }
+}
